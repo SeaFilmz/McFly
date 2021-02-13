ddbb661abf28d2639d40e7dd4b5c903398807f12
SeaFilmz <danielschmidt18@gmail.com>
2021-02-12 21:36:15 -0800

    Added Parser
    Added a parser to McFly.

--- mcfly.py
+++ mcfly.py
@@ -290,3 +290,97 @@ class EqualNode:
 
   def __repr__(self): 
     return f"({self.node_x}={self.node_y})"
+
+class Parser:
+  def __init__(self, tokens):
+    self.tokens = iter(tokens)
+    self.advance()
+
+  def raise_error(self):
+    raise Exception("Invalid Syntax")
+
+  def advance(self):
+    try:
+      self.current_token = next(self.tokens)
+    except StopIteration:
+      self.current_token = None
+
+  def parse(self):
+    if self.current_token == None:
+      return None
+
+    result = self.expr()
+
+    if self.current_token != None:
+      self.raise_error()
+
+    return result
+
+  def expr(self):
+    result = self.term()
+
+    while self.current_token != None and self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
+      if self.current_token.type == TokenType.PLUS:
+        self.advance()
+        result = AddNode(result, self.term())
+      elif self.current_token.type == TokenType.MINUS:
+        self.advance()
+        result = SubtractNode(result, self.term())
+
+    return result
+
+  def term(self):
+    result = self.factor()
+
+    while self.current_token != None and self.current_token.type in (TokenType.MULTIPLY, TokenType.DIVIDE):
+      if self.current_token.type == TokenType.MULTIPLY:
+        self.advance()
+        result = MultiplyNode(result, self.factor())
+      elif self.current_token.type == TokenType.DIVIDE:
+        self.advance()
+        result = DivideNode(result, self.factor())
+
+    return result
+
+  def factor(self):
+    token = self.current_token
+
+    if token.type == TokenType.LPAREN:
+      self.advance()
+      result = self.expr()
+
+      if self.current_token.type != TokenType.RPAREN:
+        self.raise_error()
+      
+      self.advance()
+      return result
+
+    elif token.type == TokenType.INTEGER:
+      self.advance()
+      return IntNode(token.value)
+    elif token.type == TokenType.FLOAT:
+      self.advance()
+      return FloatNode(token.value)
+    elif token.type == TokenType.PLUS:
+      self.advance()
+      return PlusNode(self.factor())
+    elif token.type == TokenType.MINUS:
+      self.advance()
+      return MinusNode(self.factor())
+    elif token.type == TokenType.STRING:
+      self.advance()
+      return StringNode(token.value)
+    elif token.type == TokenType.NUMBER_VAR:
+      self.advance()
+      return NumberSignNode(token.value)
+    elif token.type == TokenType.STRING_VAR:
+      self.advance()
+      return StringSignNode(token.value)
+    elif token.type == TokenType.ARRAY_VAR:
+      self.advance()
+      return ArraySignNode(token.value)
+    elif token.type == TokenType.EQUAL:
+      self.advance()
+      return EqualNode(token.node_y)
+        
+    self.raise_error()
