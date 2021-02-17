79b46071440891d96da5e682f98a156612436183
SeaFilmz <danielschmidt18@gmail.com>
2021-02-16 22:45:37 -0800

    Refactored Variable/Array Names Code
    Refactored the variable and array names code so that McFly keeps the same functionality but removes unnecessary code.

--- mcfly.py
+++ mcfly.py
@@ -117,61 +117,34 @@ class Lexer:
     return Token(TokenType.STRING, str(string_str))
 
   def generate_num_var(self):
-    number_sign_count = 0
     num_sign_var = self.current_char
     self.advance()
 
     while self.current_char != None and self.current_char in LETTERS_DIGITS_US:
-      if self.current_char == '#': 
-        number_sign_count += 1
-        if number_sign_count > 1:
-          break        
-    
       num_sign_var += self.current_char
       self.advance()
-
-    if num_sign_var.startswith('#') and number_sign_count == 1:
-      num_sign_var = num_sign_var
-
-    return Token(TokenType.NUMBER_VAR, str(num_sign_var))
+    
+    return Token(TokenType.NUMBER_VAR, num_sign_var)
 
   def generate_str_var(self):
-    string_sign_count = 0
     str_sign_var = self.current_char
     self.advance()
 
     while self.current_char != None and self.current_char in LETTERS_DIGITS_US:
-      if self.current_char == '$': 
-        string_sign_count += 1
-        if string_sign_count > 1:
-          break
-
       str_sign_var += self.current_char
       self.advance()
 
-    if str_sign_var.startswith('$'):
-      str_sign_var = str_sign_var
-
-    return Token(TokenType.STRING_VAR, str(str_sign_var))
+    return Token(TokenType.STRING_VAR, str_sign_var)
 
   def generate_array_var(self):
-    array_sign_count = 0
     array_sign_var = self.current_char
     self.advance()
 
     while self.current_char != None and self.current_char in LETTERS_DIGITS_US:
-      if self.current_char == '@': 
-        array_sign_count += 1
-        if array_sign_count > 1:
-          break
-
       array_sign_var += self.current_char
       self.advance()
 
-    if array_sign_var.startswith('@'):
-      array_sign_var = array_sign_var
-
-    return Token(TokenType.ARRAY_VAR, str(array_sign_var))
+    return Token(TokenType.ARRAY_VAR, array_sign_var)
 
   def make_equal(self):
     equal_sign_count = 0
