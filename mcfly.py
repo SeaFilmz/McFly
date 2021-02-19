526011c5721702eafdcb0007b3469d3edec5ce07
SeaFilmz <danielschmidt18@gmail.com>
2021-02-19 12:59:43 -0800

    Refactored #pi/#e Command Code
    Refactored the #pi and #e command code by deleting some duplicated code.

--- mcfly.py
+++ mcfly.py
@@ -171,8 +171,6 @@ class Lexer:
 class Token:
   type: TokenType
   value: any = None
-  StartValuePi = important_numbers['#pi']
-  StartValueE = important_numbers['#e']
 
   def __repr__(self):
     return self.type.name + ((f":{self.value}") if self.value != None else "")
