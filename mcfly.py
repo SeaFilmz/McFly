bfaaa7a0a691640c84a6a0102636723907f61622
SeaFilmz <danielschmidt18@gmail.com>
2021-07-16 22:23:48 -0700

    Refactor visit_LessThanEqualNode Function More
    Refactor code for the visit_LessThanEqualNode function in the interpreter by moving (check_x, float) and (check_y, float) to the same line as (check_x, int) and (check_y, int) with an or. Also, merger the if and elif statements together with an or. This reduced lines of code.

--- mcfly.py
+++ mcfly.py
@@ -638,19 +638,12 @@ class Interpreter:
     check_x = self.visit(node.node_x).value
     check_y = self.visit(node.node_y).value
 
-    if isinstance(check_x, int) and isinstance(check_y, int):
-      if int(check_x) < int(check_y):
+    if (isinstance(check_x, int) and isinstance(check_y, int)) or (isinstance(check_x, float) and isinstance(check_y, float)):
+      if (int(check_x) < int(check_y)) or (float(check_x) < float(check_y)):
         return 'True'
-      elif int(check_x) > int(check_y):
+      elif (int(check_x) > int(check_y)) or (float(check_x) > float(check_y)):
         return 'False'
-      elif int(check_x) == int(check_y):
-        return 'True'
-    elif isinstance(check_x, float) and isinstance(check_y, float):
-      if float(check_x) < float(check_y):
-        return 'True'
-      elif float(check_x) > float(check_y):
-        return 'False'
-      elif float(check_x) == float(check_y):
+      elif (int(check_x) == int(check_y)) or (float(check_x) == float(check_y)):
         return 'True'
     elif (isinstance(check_x, int) and isinstance(check_y, float)) or isinstance(check_x, float) and isinstance(check_y, int):
       if (int(check_x) < float(check_y)) or (float(check_x) < int(check_y)):
