e4edab685427d95588b359e62d7180d03141435c
SeaFilmz <danielschmidt18@gmail.com>
2021-07-18 16:43:16 -0700

    Refactor visit_NotEqualNode Function Again
    Refactor code for the visit_NotEqualNode function in the interpreter by moving the isinstance check for ((check_x, int) and (check_y, float)) or ((check_x, float) and (check_y, int)) to the same line as isinstance for ((check_x, int) and (check_y, int)) or ((check_x, float) and (check_y, float)) with an or. Also, merger the other if and elif statements together. This reduced lines of code.

--- mcfly.py
+++ mcfly.py
@@ -669,16 +669,11 @@ class Interpreter:
     check_x = self.visit(node.node_x).value
     check_y = self.visit(node.node_y).value
 
-    if (isinstance(check_x, int) and isinstance(check_y, int)) or (isinstance(check_x, float) and isinstance(check_y, float)):
+    if (isinstance(check_x, int) and isinstance(check_y, int)) or (isinstance(check_x, float) and isinstance(check_y, float)) or (isinstance(check_x, int) and isinstance(check_y, float)) or (isinstance(check_x, float) and isinstance(check_y, int)):
       if check_x != check_y:
         return 'True'
       elif check_x == check_y:
         return 'False'
-    elif (isinstance(check_x, int) and isinstance(check_y, float)) or (isinstance(check_x, float) and isinstance(check_y, int)):
-      if check_x == check_y:
-        return 'False'
-      elif check_x != check_y:
-        return 'True'
 
   def visit_AddNode(self, node):
     check_num_a = self.visit(node.node_a).value
