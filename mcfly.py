4a19b29f8b1635ac34449d24e2d07f0d84cde08d
SeaFilmz <danielschmidt18@gmail.com>
2021-07-17 23:49:26 -0700

    Refactor visit_LessThanNode Function Again
    Refactor code for the visit_LessThanNode function in the interpreter by moving the isinstance check for ((check_x, int) and (check_y, float)) or ((check_x, float) and (check_y, int)) to the same line as isinstance for ((check_x, int) and (check_y, int)) or ((check_x, float) and (check_y, float)) with an or. Also, merger the if and elif statements together with an or. This reduced lines of code.

--- mcfly.py
+++ mcfly.py
@@ -638,14 +638,7 @@ class Interpreter:
     check_x = self.visit(node.node_x).value
     check_y = self.visit(node.node_y).value
 
-    if (isinstance(check_x, int) and isinstance(check_y, int)) or (isinstance(check_x, float) and isinstance(check_y, float)):
-      if (check_x > check_y):
-        return 'False'
-      elif (check_x < check_y):
-        return 'True'
-      elif (check_x == check_y):
-        return 'False'
-    elif (isinstance(check_x, int) and isinstance(check_y, float)) or (isinstance(check_x, float) and isinstance(check_y, int)):
+    if (isinstance(check_x, int) and isinstance(check_y, int)) or (isinstance(check_x, float) and isinstance(check_y, float)) or (isinstance(check_x, int) and isinstance(check_y, float)) or (isinstance(check_x, float) and isinstance(check_y, int)):
       if (check_x > check_y):
         return 'False'
       elif (check_x < check_y):
