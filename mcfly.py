2826f818ae36af1899d5c8f6ede9877e1decc309
SeaFilmz <danielschmidt18@gmail.com>
2021-07-20 00:05:27 -0700

    Reduce Usage of isinstance in visit_NotEqualNode
    Refactor visit_NotEqualNode function code by reducing the number of times I use the isinstance built in function in python.

--- mcfly.py
+++ mcfly.py
@@ -685,7 +685,7 @@ class Interpreter:
     check_x = self.visit(node.node_x).value
     check_y = self.visit(node.node_y).value
 
-    if (isinstance(check_x, int) and isinstance(check_y, int)) or (isinstance(check_x, float) and isinstance(check_y, float)) or (isinstance(check_x, int) and isinstance(check_y, float)) or (isinstance(check_x, float) and isinstance(check_y, int)):
+    if (isinstance(check_x, int) or isinstance(check_x, float)) and (isinstance(check_y, int) or isinstance(check_y, float)):
       if check_x != check_y:
         return 'True'
       elif check_x == check_y:
