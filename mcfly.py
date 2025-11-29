cd642150cf0e2ac5356c45a489e75025299f430d
SeaFilmz <danielschmidt18@gmail.com>
2025-11-29 14:26:39 -0800

    Move CEIL keyword to isalpha() check in the generate_tokens() in the lexer
    Removed generate_ceil() and single-letter 'c' check. CEIL keyword is now handled in the main isalpha() block for consistency and simpler lexer structure.

--- mcfly.py
+++ mcfly.py
@@ -174,6 +174,8 @@ class Lexer:
           yield Token(TokenType.FLOAT_TYPE)
         elif upper_word == "STR?":
           yield Token(TokenType.STRING_TYPE)
+        elif upper_word == "CEIL":
+          yield Token(TokenType.CEIL)
       elif self.current_char == 'a':
         yield self.generate_a_keywords()
       elif self.current_char == 'o':
@@ -190,8 +192,6 @@ class Lexer:
         yield self.generate_s_keywords()
       elif self.current_char == 'e':
         yield self.generate_even()
-      elif self.current_char == 'c':
-        yield self.generate_ceil()
       elif self.current_char in LETTERS:
         yield self.generate_error_words()
       else:
@@ -481,19 +481,6 @@ class Lexer:
           return Token(TokenType.EVEN_CHECK)
     else:
       return Token(TokenType.ERROR_WORDS, self.show_error_words('e'))
-
-  def generate_ceil(self):
-    self.advance()
-    if self.current_char == 'e':
-      self.advance()
-      if self.current_char == 'i':
-        self.advance()
-        self.lastCharCheckAdvance('l')
-        if self.current_char in LETTERS:
-          return Token(TokenType.ERROR_WORDS, self.show_error_words('ceil'))
-        return Token(TokenType.CEIL)
-    else:
-      return Token(TokenType.ERROR_WORDS, self.show_error_words('c'))
 
   def generate_error_words(self):
     return Token(TokenType.ERROR_WORDS, self.show_error_words(''))
