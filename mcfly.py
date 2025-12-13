b42ca187533d0b52c5936b86695999d8a00e833d
SeaFilmz <danielschmidt18@gmail.com>
2025-12-12 23:21:13 -0800

    Refactor <= comparison to return BooleanNode
    Simplifies less-than-equal logic and ensures comparisons return a BooleanNode instead of string or TrueNode/FalseNode values.

--- mcfly.py
+++ mcfly.py
@@ -1222,11 +1222,10 @@ class Interpreter:
     check_x = self.visit(node.node_x).value
     check_y = self.visit(node.node_y).value
 
-    if (isinstance(check_x, int) or isinstance(check_x, float)) and (isinstance(check_y, int) or isinstance(check_y, float)):
-      if check_x > check_y:
-        return 'False'
-      elif (check_x < check_y) or (check_x == check_y):
-        return 'True'
+    if not isinstance(check_x, (int, float)) or not isinstance(check_y, (int, float)):
+        raise Exception("<= comparison requires numeric values")
+
+    return BooleanNode(check_x <= check_y)
 
   def visit_NotEqualNode(self, node):
     check_x = self.visit(node.node_x).value
