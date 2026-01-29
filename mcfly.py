f67e8868187661c3303066e39ef1b36afcbbbb02
SeaFilmz <danielschmidt18@gmail.com>
2026-01-28 23:59:52 -0800

    Refactor visit_LessThanNode() to return boolean result for conditional evaluation
    Removes BooleanNode wrapping and returns a direct True/False value so less-than comparisons work properly inside conditionals.

--- mcfly.py
+++ mcfly.py
@@ -1984,14 +1984,13 @@ class Interpreter:
     return check_x > check_y
 
   def visit_LessThanNode(self, node):
-    check_x = self.visit(node.node_x).value
-    check_y = self.visit(node.node_y).value
+    check_x = self.visit(node.node_x)
+    check_y = self.visit(node.node_y)
 
-    if (isinstance(check_x, int) or isinstance(check_x, float)) and (isinstance(check_y, int) or isinstance(check_y, float)):
-      if (check_x > check_y) or (check_x == check_y):
-        return BooleanNode(False)
-      elif check_x < check_y:
-        return BooleanNode(True)
+    if not isinstance(check_x, (int, float)) or not isinstance(check_y, (int, float)):
+      raise Exception("LessThanNode requires numeric values")
+
+    return check_x < check_y
 
   def visit_GreaterThanEqualNode(self, node):
     check_x = self.visit(node.node_x).value
